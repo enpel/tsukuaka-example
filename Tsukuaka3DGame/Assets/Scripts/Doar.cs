f07b2bc5c2cf48b9ea553ee2ffb5907a010baa22
enpel <enpel715@gmail.com>
2021-01-04 08:56:22 +0900

    ドアのギミックを追加した

---
+++ Tsukuaka3DGame/Assets/Scripts/Doar.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doar : MonoBehaviour
+{
+    public void Open()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
---
+++ Tsukuaka3DGame/Assets/Scripts/OpenDoarTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoarTrigger : MonoBehaviour
+{
+    [SerializeField] Doar doar;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            doar.Open();
+        }
+    }
+}
