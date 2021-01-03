2890f772d99474a3ebe9d868a69ff4652965b245
enpel <enpel715@gmail.com>
2021-01-04 08:33:16 +0900

    add Timer

---
+++ Tsukuaka3DGame/Assets/Scripts/TimeCounter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TimeCounter : MonoBehaviour
+{
+    [SerializeField] float limitTime = 30; //秒
+
+    float currentTime = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = limitTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= Time.deltaTime;
+
+        if (currentTime < 0)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
