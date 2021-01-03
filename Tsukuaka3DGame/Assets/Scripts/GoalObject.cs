7da0d406d4121c274ec303745a4e3bd7dccababf
enpel <enpel715@gmail.com>
2021-01-04 08:27:45 +0900

    イライラ棒が作れるようになった

---
+++ Tsukuaka3DGame/Assets/Scripts/GoalObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoalObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("GameClear");
+        }
+    }
+}
---
+++ Tsukuaka3DGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal"); // 横方向の入力が-1～+1
+        var z = Input.GetAxis("Vertical");　// 縦方向の入力が-1～+1
+
+        this.transform.Translate(new Vector3(x, 0, z) * Time.deltaTime * runSpeed);
+    }
+}
