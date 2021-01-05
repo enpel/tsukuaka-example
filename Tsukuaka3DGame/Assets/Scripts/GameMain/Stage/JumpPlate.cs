450760604afff640bd3384a171cc48f1cf67040b
enpel <enpel715@gmail.com>
2021-01-06 03:22:22 +0900

    ジャンプ台を追加してみた

---
+++ Tsukuaka3DGame/Assets/Scripts/GameMain/Stage/JumpPlate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPlate : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.IsPlayerObject())
+        {
+            other.gameObject.GetComponent<Rigidbody>()?.AddForce(Vector3.up * 10, ForceMode.Impulse);
+        }
+    }
+}
