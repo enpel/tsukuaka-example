38b855e6717a64605deb419e7e55ebe4d49846d8
enpel <enpel715@gmail.com>
2021-01-05 22:32:01 +0900

    タイトル画面の作成

---
+++ Tsukuaka3DGame/Assets/Scripts/Title/TitleController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleController : MonoBehaviour
+{
+    public void GameStart()
+    {
+        SceneManager.LoadScene("GameMain");
+    }
+}
