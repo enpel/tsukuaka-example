d1a4b429492f9f44caac9e7d79dbfa8e0074491f
enpel <enpel715@gmail.com>
2021-01-05 23:19:00 +0900

    Playerをゲーム開始時に生成

---
+++ Tsukuaka3DGame/Assets/Scripts/GameMain/StageDirector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageDirector : MonoBehaviour
+{
+    [SerializeField] TimeCounter timeCounter;
+    [SerializeField] Transform spawnPoint;
+    [SerializeField] GameObject playerPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
+}
