a7f059bd36859005625b3878b42ac07fcfb8cc13
enpel <enpel715@gmail.com>
2021-01-05 23:23:31 +0900

    メインに入ったらカウントダウンする

--- Tsukuaka3DGame/Assets/Scripts/GameMain/StageDirector.cs
+++ Tsukuaka3DGame/Assets/Scripts/GameMain/StageDirector.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StageDirector : MonoBehaviour
 {
     [SerializeField] TimeCounter timeCounter;
     [SerializeField] Transform spawnPoint;
     [SerializeField] GameObject playerPrefab;
+    [SerializeField] Text centerText;
     // Start is called before the first frame update
     void Start()
     {
-        GameObject.Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+        StartCoroutine(GameStart());
     }
 
+    IEnumerator GameStart()
+    {
+        yield return new WaitForSeconds(1f);
+        centerText.gameObject.SetActive(true);
+        centerText.text = "3";
+        yield return new WaitForSeconds(1f);
+        centerText.text = "2";
+        GameObject.Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+        yield return new WaitForSeconds(1f);
+        centerText.text = "1";
+        yield return new WaitForSeconds(1f);
+        centerText.gameObject.SetActive(false);
+    }
 }
