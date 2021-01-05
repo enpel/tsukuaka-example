0eef86382caf9a13b964da02915035f1b019ce78
enpel <enpel715@gmail.com>
2021-01-05 22:05:56 +0900

    多段ジャンプの防止

--- Tsukuaka3DGame/Assets/Scripts/PlayerController.cs
+++ Tsukuaka3DGame/Assets/Scripts/PlayerController.cs
@@ -6,23 +6,41 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float runSpeed = 3.0f;
 
+    bool _isJumping = false;
+    Rigidbody _rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody = this.GetComponent<Rigidbody>();
+
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         var x = Input.GetAxis("Horizontal"); // 横方向の入力が-1～+1
         var z = Input.GetAxis("Vertical");　// 縦方向の入力が-1～+1
 
         this.transform.Translate(new Vector3(x, 0, z) * Time.deltaTime * runSpeed);
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !_isJumping)
         {
-            this.GetComponent<Rigidbody>().AddForce(Vector3.up * 300);
+            _rigidbody.AddForce(Vector3.up * 300);
+            _isJumping = true;
+        }
+
+        if (_isJumping && _rigidbody.velocity.y < 0)
+        {
+            RaycastHit hit;
+            // Does the ray intersect any objects excluding the player layer
+            if (Physics.Raycast(transform.position, Vector3.down, out hit))
+            {
+                if (hit.distance < 1.05f)
+                {
+                    _isJumping = false;
+                }
+            }
         }
     }
 }
